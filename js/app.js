4743710e28b25234c9d623cb8fda607da74775d4
yalda <yalda.tsi@gmail.com>
2022-03-16 10:35:11 +0330

    final

--- js/app.js
+++ js/app.js
@@ -4,9 +4,15 @@ const pageBtn = document.querySelector('.pages-btn')
 const homedropDown = document.querySelector('#dropdown-content-home')
 const activityDropDown = document.querySelector('#dropdown-content-activity')
 const pagesDropDown = document.querySelector('#dropdown-content-pages')
+const email = document.querySelector('#email')
+const submitBtn = document.querySelector('.sumbit')
 
 const showMeu = (menu) => {
   menu.classList.toggle("show");
+}
+const hideMenu = (menu) => {
+  menu.classList.toggle("show");
+
 }
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
@@ -30,13 +36,37 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+const validateEmail = () => {
+  let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if(email.value.match(mailFormat)) {
+    alert("We Will recieve new updates in your e-mail soon!")
+  }else if(email.value === ""){
+    alert('please fill the email section')
+  }else {
+    alert('You have entered an invalid Email address!')
+
+  }
+}
+
 window.addEventListener("scroll", reveal);
+
 homeBtn.addEventListener('mouseover',() => {
   showMeu(homedropDown)
 })
+homeBtn.addEventListener('mouseout',() => {
+  hideMenu(homedropDown)
+})
+
 activityBtn.addEventListener('mouseover',()=> {
 showMeu(activityDropDown)
+})
+activityBtn.addEventListener('mouseout',() => {
+  hideMenu(activityDropDown)
 })
 pageBtn.addEventListener('mouseover', () => {
   showMeu(pagesDropDown)
 })
+pageBtn.addEventListener('mouseout',() => {
+  hideMenu(pagesDropDown)
+})
+submitBtn.addEventListener('click',validateEmail)
